76bfa3e0c85e72404efacd91e62753d3bddd23a6
scngnr <scngnr@gmail.com>
2023-01-31 21:53:13 +0300

    ek

---
+++ src/Provider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Scngnr\Parasut;
+
+use Illuminate\Support\ServiceProvider;
+
+class Provider extends ServiceProvider
+{
+  public function register()
+  {
+    //
+  }
+
+  public function boot()
+  {
+
+    $this->loadMigrationsFrom(__DIR__ . '/Database/migrations');   //Database migration
+    $this->loadRoutesFrom(__DIR__.'/Routes/web.php');              //Route
+    // $this->loadViewsFrom(__DIR__.'/views', 'priceService');     //Views klasörü
+  }
+}
